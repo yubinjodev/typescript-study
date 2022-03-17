726071bfd4b0b20e6db28be56d10a9a71503d408
yubin <yubin@itdadev.com>
2022-03-17 16:07:42 +0900

    create todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import InputField from "./components/InputField";
+import { Todo } from "./model";
 
-function App() {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/InputField.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+  return (
+    <form className="input">
+      <input
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        className="input__box"
+        type="input"
+        placeholder="Enter a task"
+      />
+      <button className="input_submit" type="submit">
+        Go
+      </button>
+    </form>
+  );
+};
+
+export default InputField;
