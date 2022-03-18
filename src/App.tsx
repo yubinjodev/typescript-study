cf2d8d3290b91208f11375343641e7a3d073d6ff
yubin <yubin@itdadev.com>
2022-03-18 12:12:09 +0900

    friday study

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,16 @@ import { Todo } from "./model";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
-  const [todos, setTodos] = useState<Todo[]>([])
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e:React.FormEvent) => {
+    e.preventDefault();
+  };
 
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
 };
